e3c48e431d4e3c2859a3fe7a83d19dafd7927f32
Mateusz Stempniewicz <mateusz.stempniewicz@gmail.com>
2017-08-08 10:07:43 +0200

    Add task to build html files

---
+++ simple_configuration_1/README.md
@@ -0,0 +1,18 @@
+# angular-basic-project-structure
+
+![Xcactus](https://img.shields.io/badge/Company-Xcactus-ff00f4.svg)
+![Intern](https://img.shields.io/badge/Level-Intern-brightgreen.svg)
+
+![Angular](https://img.shields.io/badge/Angular-1.6.*-red.svg)
+[![License](https://img.shields.io/badge/license-MIT-blue.svg)](https://opensource.org/licenses/MIT)
+
+## Simple configuration 1
+
+1. Opis
+2. Zależności
+3. Przykład uruchomienia
+4. Prosty tutorial
+
+- Przed rozpoczęciem pracy warto sprawdzić czy wersje pakietów gulpa się nie zmieniły i czy domyślna ich konfiguracja jest cały czas taka sama.
+
+
---
+++ simple_configuration_1/frontend/gulp/html.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp');
+var htmlMin = require('gulp-htmlmin');
+
+var path = require('./path');
+
+var buildHtml = function(production) {
+  var htmlFiles = gulp.src(path.html + '/**/*.html');
+
+  if (production) {
+    htmlFiles = htmlFiles.pipe(htmlMin({
+      collapseWhitespace: true,
+      removeComments: true,
+      removeEmptyAttributes: true
+    }));
+  }
+
+  htmlFiles = htmlFiles.pipe(gulp.dest(path.build.html));
+  return htmlFiles;
+}
+
+gulp.task('html:dev', function() {
+  return buildHtml(false);
+});
+
+gulp.task('html:production', function() {
+  return buildHtml(true);
+});
